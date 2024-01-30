49bd11ed45665429a77ba8e975495572e35a6636
Mac <mac.gray@duke.edu>
2024-01-30 00:07:45 -0500

    added admin superuser and models to admin index

--- docker-deploy/web-app/rideshare/admin.py
+++ docker-deploy/web-app/rideshare/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Driver, Rider, Ride, RideUser
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Driver)
+admin.site.register(Rider)
+admin.site.register(Ride)
+admin.site.register(RideUser)
