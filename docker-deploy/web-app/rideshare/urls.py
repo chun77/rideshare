e58fe3d1cb4a0a00bc89832353ba237dd6fec697
Mac <mac.gray@duke.edu>
2024-01-30 01:33:20 -0500

    got views initially set up

--- docker-deploy/web-app/rideshare/urls.py
+++ docker-deploy/web-app/rideshare/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name = "rideshare"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:user_id>/", views.detail, name="detail"),
 ]
--- docker-deploy/web-app/rideshare/views.py
+++ docker-deploy/web-app/rideshare/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import User
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello, world. This is the rideshare index.")
+    user_list = User.objects.all()
+    context = {
+        "user_list": user_list,
+    }
+    return render(request, "rideshare/index.html", context)
+
+def detail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, "rideshare/detail.html", {"user": user})
