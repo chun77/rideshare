3c1842b1812188ec598f7287fcd084dc9b472576
Mac <mac.gray@duke.edu>
2024-01-29 23:49:58 -0500

    added str methods to models

--- docker-deploy/web-app/rideshare/models.py
+++ docker-deploy/web-app/rideshare/models.py
@@ -10,6 +10,9 @@ class User(models.Model):
     last_name_text = models.CharField(max_length=200)
     password_text = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f'({self.first_name_text}, {self.last_name_text})'
+
 def get_default_driver():
     return Driver.object.get(name='default')
 
@@ -20,11 +23,17 @@ class Driver(models.Model):
     plate_number = models.CharField(max_length=200)
     special_info = models.TextField()
 
+    def __str__(self):
+        return f'({self.user}, {self.vehicle_type}, {self.max_passengers}, {self.plate_number}, {self.special_info})'
+
 def get_default_rider():
     return Rider.object.get(name='default')
 
 class Rider(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user}'
 
 
 class Ride(models.Model):
@@ -39,9 +48,13 @@ class Ride(models.Model):
     driver_user = models.ForeignKey(Driver, on_delete=models.SET(get_default_driver))
     request_user = models.ForeignKey(Rider, on_delete=models.SET(get_default_rider))
 
+    def __str__(self):
+        return f'({self.status}, {self.driver_user}, {self.request_user}, {self.ride_id})'
+
 
 class RideUser(models.Model):
     ride_id = models.ForeignKey(Ride, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     
-    
+    def __str__(self):
+        return f'({self.user_id}, {self.ride_id})'
