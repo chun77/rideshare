62126fa7e20fe433373ac57e37fbb4d5d6024c65
Mac <mac.gray@duke.edu>
2024-01-29 20:00:03 -0500

    postgres setup complete

--- docker-deploy/web-app/project1/settings.py
+++ docker-deploy/web-app/project1/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ridesharedb',
+        'USER': 'ridesharedbuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
