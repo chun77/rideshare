f24f78e3ddb241ac6c456301a1276861e1a807c5
Mac <mac.gray@duke.edu>
2024-01-30 22:46:04 -0500

    added generic views

--- docker-deploy/web-app/rideshare/urls.py
+++ docker-deploy/web-app/rideshare/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 app_name = "rideshare"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:user_id>/", views.detail, name="detail"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
 ]
--- docker-deploy/web-app/rideshare/views.py
+++ docker-deploy/web-app/rideshare/views.py
@@ -5,15 +5,20 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import User
 from django.http import Http404
+from django.views import generic
+
 from django.shortcuts import render, get_object_or_404
 
-def index(request):
-    user_list = User.objects.all()
-    context = {
-        "user_list": user_list,
-    }
-    return render(request, "rideshare/index.html", context)
 
-def detail(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    return render(request, "rideshare/detail.html", {"user": user})
+class IndexView(generic.ListView):
+    template_name = "rideshare/index.html"
+    context_object_name = "user_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return User.objects.all()
+
+class DetailView(generic.DetailView):
+    model = User
+    template_name = "rideshare/detail.html"
+
