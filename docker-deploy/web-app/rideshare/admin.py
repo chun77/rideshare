3eedb4479b6b6823bc2b5a75e6c6496feca7ef15
Mac <mac.gray@duke.edu>
2024-01-31 23:25:13 -0500

    admin cleanup

--- docker-deploy/web-app/rideshare/admin.py
+++ docker-deploy/web-app/rideshare/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import User, Driver, Rider, Ride, RideUser
 # Register your models here.
-admin.site.register(User)
+
 admin.site.register(Driver)
 admin.site.register(Rider)
 admin.site.register(Ride)
 admin.site.register(RideUser)
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ["first_name_text", "last_name_text"]
+    list_display = ["first_name_text", "last_name_text"]
+    search_fields = ["first_name_text", "last_name_text"]
+
+admin.site.register(User, UserAdmin)
