e11978cbc11c875caa8e0296946674e6ff9c9e97
Angelina <zw297@duke.edu>
2024-02-03 00:50:12 -0500

    riderequestdone

--- docker-deploy/web-app/rideshare/admin.py
+++ docker-deploy/web-app/rideshare/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Driver, Rider, Ride, RideUser
+from .models import Driver, Ride, RideUser
 # Register your models here.
 from django.contrib.auth.models import User
 
 
 admin.site.register(Driver)
-admin.site.register(Rider)
+# admin.site.register(Rider)
 admin.site.register(Ride)
 admin.site.register(RideUser)
 
--- docker-deploy/web-app/rideshare/views.py
+++ docker-deploy/web-app/rideshare/views.py
@@ -7,12 +7,13 @@ from .models import *
 from django.http import Http404
 from django.views import generic
 from django.contrib import messages
-from .forms import CreateUserForm, CreateDriverForm
+from .forms import CreateUserForm, CreateDriverForm, CreateRideForm
 from django.contrib.auth import authenticate, login, logout
-from .models import Driver, Ride, Rider, RideUser
+from .models import Driver, Ride, RideUser
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.edit import UpdateView
+from datetime import datetime
 
 
 # class IndexView(generic.ListView):
@@ -116,5 +117,28 @@ def home(request):
 
 @login_required(login_url='rideshare:login')
 def rideRequest(request):
-	context = {}
+	form = CreateRideForm()
+	
+	if request.method == 'POST':
+		form = CreateRideForm(request.POST)
+		if form.is_valid():
+			
+
+			ride = form.save(commit=False)
+
+			date_str = request.POST['d']  # e.g., '2024-02-03'
+			time_str = request.POST['e']  # e.g., '14:30'
+			# Combine the date and time strings into a datetime object
+			ride.arrival_time = datetime.strptime(f'{date_str} {time_str}', '%Y-%m-%d %H:%M')
+			
+    
+			ride.status = "OPEN"
+			ride.request_user = request.user
+			ride.save()
+			rideuser = RideUser.objects.create(ride = ride,
+								user = request.user, 
+								num_party = ride.num_passengers)
+			rideuser.save()
+			return redirect('rideshare:home')
+	context = {'form': form}
 	return render(request, 'rideshare/rideRequest.html', context)
